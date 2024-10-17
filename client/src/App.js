41b021f5c801ff294bb2b50c914127c2ab94de6f
Abhishek kumar <thecode.abhi@gmail.com>
2024-10-17 18:33:33 +0530

    iNITIAL

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import {ThemeProvider,styled} from "styled-components";
+import { lightTheme, LightTheme} from "./utils/Themes.js";
+import {BrowserRouter} from "react-router-dom";
+// import { Container } from "@mui/material";
+// import styled from "styled-components";
 
-const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Container = styled.div`
+width:100%;
+height:100vh;
+display:flex;
+flex-direction:column;
+background: ${({theme})=>theme.bg};
+`;
+
+function App() {
+  return <ThemeProvider theme={lightTheme}>
+    <BrowserRouter>
+      <Container>Hi</Container>
+    </BrowserRouter>
+  </ThemeProvider>
+
 }
 
-export default App
+export default App;
